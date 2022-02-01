89506f89f77d55bf36c48fff62108da9c004cd34
Alecto Irene Perez <perez.cs@pm.me>
2022-01-31 18:43:41 -0700

    Add support for reading from stdin
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,51 @@
 use std::env;
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Stdin};
 use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 2 {
-        let program_name = &args[0];
-        println!("Usage:\n\t{program_name} <filename>")
-    } else {
-        let filename = &args[1];
-        match read_lines(filename) {
-            Ok(lines) => {
-                println!("      │ File: {filename}");
-                println!("──────┼────────────────────────────────");
-                for (i, result) in lines.enumerate() {
-                    match result {
-                        Ok(line) => println!("{i:5} │ {line}"),
-                        Err(err) => println!("Error: {err}"),
-                    }
-                }
-            }
-            Err(err) => {
-                println!("Unable to open '{filename}': {err}")
-            }
+    match args.len() {
+        1 => process_lines("(standard input)", read_stdin()),
+        2 => match args[1].as_ref() {
+            "-" => process_lines("(standard input)", read_stdin()),
+            filename => match read_lines(filename) {
+                Ok(lines) => process_lines(filename, lines),
+                Err(err) => println!("Unable to open '{filename}': {err}"),
+            },
+        },
+        num_inputs => {
+            let program_name = if num_inputs == 0 {
+                "<unknown>"
+            } else {
+                &args[0]
+            };
+            println!("Usage:\n\t{program_name} [filename]");
+            println!();
+            println!("If a filename is provided, reads lines from that file. Otherwise, reads lines from stdin");
         }
     }
 }
 
+fn read_stdin() -> io::Lines<io::BufReader<Stdin>> {
+    io::BufReader::new(io::stdin()).lines()
+}
+
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
+fn read_lines<P: AsRef<Path>>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+fn process_lines<T: io::BufRead>(filename: &str, lines: io::Lines<T>) {
+    println!("      │ File: {filename}");
+    println!("──────┼────────────────────────────────");
+    for (i, result) in lines.enumerate() {
+        match result {
+            Ok(line) => println!("{i:5} │ {line}"),
+            Err(err) => println!("Error: {err}"),
+        }
+    }
 }
