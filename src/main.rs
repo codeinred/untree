91efc2abeb5689da9a7c4dc8fb821fb0f486e455
Alecto Irene Perez <perez.cs@pm.me>
2022-01-31 19:03:14 -0700

    Clean up type signatures
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::fs::File;
-use std::io::{self, BufRead, Stdin};
+use std::io::{self, BufRead, BufReader, Error as ioError, Lines, Stdin};
 use std::path::Path;
 
 fn main() {
@@ -35,18 +35,18 @@ fn print_help(args: Vec<String>) {
     println!("If the filename is '-', standard input is used as the file.");
 }
 
-fn read_stdin() -> io::Lines<io::BufReader<Stdin>> {
+fn read_stdin() -> Lines<BufReader<Stdin>> {
     io::BufReader::new(io::stdin()).lines()
 }
 
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P: AsRef<Path>>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> {
+fn read_lines<P: AsRef<Path>>(filename: P) -> Result<Lines<BufReader<File>>, ioError>
+{
     File::open(filename).map(|file| io::BufReader::new(file).lines())
 }
 
-
-fn process_lines<T: io::BufRead>(filename: &str, lines: io::Lines<T>) {
+fn process_lines(filename: &str, lines: Lines<impl BufRead>) {
     println!("      │ File: {filename}");
     println!("──────┼────────────────────────────────");
     for (i, result) in lines.enumerate() {
