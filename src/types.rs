3887f26c0cf19bb1ffa99e72d7c5b8b0ae483795
Alecto Irene Perez <perez.cs@pm.me>
2022-02-01 23:40:54 -0700

    Format help message better by including clap wrap_help
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- src/types.rs
+++ src/types.rs
@@ -18,24 +18,21 @@ impl UntreeOptions {
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 pub struct Args {
-    /// Directory in which to generate tree
-    ///
-    /// (Uses current working directory if no directory is specified)
+    /// Directory to use as the root of the newly generated directory structure.
+    /// Uses current working directory if no directory is specified.
     #[clap(short, long)]
     pub dir: Option<String>,
-    /// List of files containing trees to be read by untree
-    ///
-    /// (If no files are specified then the tree is read from stdin)
+    /// List of files containing trees to be read by untree. If no files are
+    /// specified, then the tree is read from standard input.
     #[clap()]
     pub tree_files: Vec<String>,
 
     /// Print the names of files and directories without creating them.
-    ///
     /// Implies verbose.
     #[clap(long)]
     pub dry_run: bool,
 
-    /// Print out the names of files and directories that untree creates
+    /// Print out the names of files and directories that untree creates.
     #[clap(short, long)]
     pub verbose: bool,
 }
