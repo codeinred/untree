0149e49d8239d77396a0d2148bfd576f3c9ad907
Alecto Irene Perez <perez.cs@pm.me>
2022-02-02 11:26:37 -0700

    Remove unwrap! macro
    This can be replaced with Option.transpose()
    
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- src/macros.rs
+++ src/macros.rs
@@ -1,22 +1,3 @@
-#[allow(unused_macros)]
-
-/**
- * Takes an Option<Result<T, Err>> and produces a Option<T>, with the error
- * being propagated by the ? operator
- */
-macro_rules! unwrap {
-    ($expression:expr) => {
-        match ($expression) {
-            Some(result) => Some(result?),
-            None => None,
-        }
-    };
-}
-
-// I like this macro and I wanna keep it
-#[allow(unused_imports)]
-pub(crate) use unwrap;
-
 /**
  * Takes a series of expressions returning Option<T>, and evaluates each one 
  * until finding an expression that succeeds
