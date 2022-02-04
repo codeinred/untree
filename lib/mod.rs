504953c573835d08c0b6e28257fef47a83daca4a
Alecto Irene Perez <perez.cs@pm.me>
2022-02-04 12:37:47 -0700

    Remove lib/traits.rs (contents were unused)
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- lib/mod.rs
+++ lib/mod.rs
@@ -2,13 +2,12 @@ pub mod either;
 pub mod errors;
 pub mod functions;
 pub mod more_context;
-pub mod traits;
-pub mod types;
 pub mod path_action;
+pub mod types;
 
 use either::either;
 pub use errors::*;
 pub use functions::*;
 pub use more_context::*;
-pub use types::*;
 pub use path_action::*;
+pub use types::*;
