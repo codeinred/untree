6578bf96efa76f5d314ffbc622f5db60a3d2ca50
Alecto Irene Perez <perez.cs@pm.me>
2022-02-04 01:36:17 -0700

    Remove usused error type code
    This code was rendered unnecessary by use of the quick-error package,
    which provided the functionality I was trying to define
    
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- lib/add_context.rs
+++
@@ -1,6 +0,0 @@
-use super::Collapse;
-/// Allows context to be added to a value, transforming it into a new type that
-/// stores the context.
-pub trait AddContext<Context, Source: Collapse<Context>, ResultType> {
-    fn add_context(self, context: Source) -> ResultType;
-}
--- lib/collapse.rs
+++
@@ -1,4 +0,0 @@
-/// Allows a type family to be collapsed into a single type
-pub trait Collapse<To> {
-    fn collapse(self) -> To;
-}
