fe5531cc4c0cd77e001733b9a87042fa885e100f
Alecto Irene Perez <perez.cs@pm.me>
2022-02-04 01:27:52 -0700

    Implement SupplyMissing for untree::Error
    This makes it easier to provide context on errors when the function
    being invoked didn't have that context.
    
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- lib/errors.rs
+++ lib/errors.rs
@@ -1,10 +1,14 @@
-use quick_error::{quick_error, ResultExt, Context};
+use quick_error::quick_error;
 use std::io;
 use std::path::{Path, PathBuf};
 
-pub enum Actions {
-    ReadStdin, ReadFile, CreateFile, CreateDir, 
+use super::SupplyMissing;
+
+pub enum ReadStdinType {
+    ReadStdin,
 }
+pub use ReadStdinType::ReadStdin;
+
 quick_error! {
     #[derive(Debug)]
     pub enum Error {
@@ -17,6 +21,26 @@ quick_error! {
         OnPath(filename: PathBuf, err: io::Error) {
             context(path: &'a Path, err: io::Error)
                 -> (path.to_path_buf(), err)
+        }
+    }
+}
+
+impl SupplyMissing<ReadStdinType> for Error {
+    fn supply_missing(self, _: ReadStdinType) -> Self {
+        use Error::*;
+        match self {
+            MissingContext(err) => OnStdin(err),
+            err => err,
+        }
+    }
+}
+
+impl<'a> SupplyMissing<&'a Path> for Error {
+    fn supply_missing(self, path: &'a Path) -> Self {
+        use Error::*;
+        match self {
+            MissingContext(err) => OnPath(path.to_path_buf(), err),
+            err => err,
         }
     }
 }
